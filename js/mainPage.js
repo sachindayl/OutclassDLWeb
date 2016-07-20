cf07424342bb686a4d2fa021e33ca7dba696269e
Sachinda <sachinda.yl@gmail.com>
2016-07-20 08:23:57 +0530

    scenario redirect according to button toggle

--- js/mainPage.js
+++ js/mainPage.js
@@ -5,6 +5,7 @@ var t1wktsafterdlcalc;
 var t2oversavailatstart;
 var t1revisedtotalcheckbox;
 
+//initializing  variables and setting them up on load.
 function init(){
 	t1CompleteBatCheck = document.getElementById('t1CompleteBatCheck');
 	t1revisedtotalcheck = document.getElementById('t1revisedtotalcheck');
@@ -20,7 +21,9 @@ function init(){
 	t2oversavailatstart.style.visibility = 'hidden';
 }
 
-
+/*Main toggle. checks if user knows team1 score. If he does not, make plans
+make the user find it. otherwise send to find team 2 interruptions.
+*/
 function t1finalscore(){
 	if(t1CompleteBatCheck.checked){
 		t1revisedtotalcheck.style.visibility = 'hidden';
@@ -36,6 +39,9 @@ function t1finalscore(){
 	}
 }
 
+/*if the revised score score is already calculated user is asked for that
+score. The text boxes will be displayed. Otherwise text boxes will be hidden
+*/
 function t1revisedscore(){
 	if(t1revisedtotalcheckbox.checked){
 		t1scoreafterdlcalc.style.visibility = '';
@@ -46,6 +52,20 @@ function t1revisedscore(){
 		t1wktsafterdlcalc.style.visibility = 'hidden';
 		t2oversavailatstart.style.visibility = 'hidden';
 	}
+}
+
+// This method selects which scenario should be seen by the user
+function pageselect(){
+	if(t1CompleteBatCheck.checked){
+		window.location.href = 'scenario1.html';
+	}else{
+		if(!t1CompleteBatCheck.checked && !t1revisedtotalcheckbox.checked){
+			window.location.href = 'scenario2.html';
+		}else{
+			window.location.href = 'scenario1.html';
+		}
+	}
+
 }
 
 // function test(){
